3e28e43767a087e7b1ff8c81fa249511d43eb04b
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2025-09-15 15:38:14 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,21 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    jest: true,
   },
-  extends: 'airbnb-base',
+  extends: "eslint:recommended",
   overrides: [
+    {
+      files: ["**/*/*.test.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-  },
+  rules: {},
 };
